c8266c2e3d73404f09153664edf4475683a61f39
Thomas Coudrat <thomas.coudrat@gmail.com>
2014-02-14 17:04:17 +1100

    Added repeatDirectory creation to vs_slice, also added arg parsing

--- vs_slice.py
+++ vs_slice.py
@@ -10,73 +10,134 @@
 #--------------------------------------------------------
 
 import os
+import argparse
+import glob
+import shutil
 
-# Ligand library, slice and repeats
-libSize = 17720
-sliceSize = 3000
-repeatNum = 3
+def main():
+    # Getting all the args
+    args = parsing()
 
-# VS parameters
-walltime = "0-24:00:00"
-thor = "5."
+    # Library params
+    libSize = int(args.libSize)
+    sliceSize = int(args.sliceSize)
+    repeatNum = int(args.repeatNum)
+    # VS params
+    walltime = args.walltime
+    thor = args.thor
+    # Project info
+    setupDir = args.setupDir
+    projName = glob.glob(setupDir + "/*.dtb")[0].replace(".dtb", "")
 
-# Project info
-projName = "ADORA2A_2YDV_allLoop"
+    # Creating the repeats directories, which are copies of the setupDir
+    createRepeats(repeatNum, setupDir)
 
-repeat = 1
-# Loop over repeat directories
-while repeat <= repeatNum:
+    # Create the .slurm slices
+    createSlices(libSize, sliceSize, repeatNum, walltime, thor, projName)
 
-    # Initialize variables for this repeat
 
-    repeatDir = os.getcwd() + "/" + str(repeat) + "/"
-    if not os.path.exists(repeatDir):
-        os.makedirs(repeatDir)
-    upperLimit = 0
-    sliceCount = 0
-    exit = False
+def parsing():
+    # Parsing descriptions for arguments
+    descr = "Slices a VS scripts into portions of library for slurm submission"
+    descr_libSize = "Size of the full library"
+    descr_sliceSize = "Size of the slices"
+    descr_repeatNum = "Number of repeats"
+    descr_walltime = "Walltime for a single slice (format: 1-24:00:00)"
+    descr_thor = "Thoroughness of the docking (format: 5.)"
+    descr_setupDir = "Name of the directory containing setup files"
 
-    # Loop over the slices
-    while True:
+    # Defining the arguments
+    parser = argparse.ArgumentParser(description=descr)
+    parser.add_argument("libSize", help=descr_libSize)
+    parser.add_argument("sliceSize", help=descr_sliceSize)
+    parser.add_argument("repeatNum", help=descr_repeatNum)
+    parser.add_argument("walltime", help=descr_walltime)
+    parser.add_argument("thor", help=descr_thor)
+    parser.add_argument("setupDir", help=descr_setupDir)
 
-        # Update upperLimit and sliceCount
-        lowerLimit = upperLimit + 1
-        upperLimit += sliceSize
-        sliceCount += 1
+    # Parsing and storing into variables
+    args = parser.parse_args()
 
-        # Exit statement of the loop, when
-        if upperLimit > libSize:
-            upperLimit = libSize
-            exit = True
+    return args
 
-        # Create sliceName for job name and slurm file name
-        sliceName = projName + "_rep" + str(repeat) + "_sl" + str(upperLimit)
 
-        # Content of the slurm file to be created
-        lines = []
-        lines.append("#!/bin/bash")
-        lines.append("#SBATCH -p main")
-        lines.append("#SBATCH --ntasks=1")
-        lines.append("#SBATCH --mem-per-cpu=1024")
-        lines.append("#SBATCH --time=" + walltime)
-        lines.append("#SBATCH --job-name=" + sliceName)
-        lines.append("")
-        lines.append("ICMHOME=/vlsci/VR0024/tcoudrat/bin/icm-3.7-3b")
-        lines.append("$ICMHOME/icm64 -vlscluster $ICMHOME/_dockScan " + projName +
-            " thorough=" + thor +
-            " from=" + str(lowerLimit) +
-            " to=" + str(upperLimit) +
-            " >& " + projName + "_" + str(upperLimit) + ".ou")
+def createRepeats(repeatNum, setupDir):
 
-        # Create the slurm file in current repeat directory
-        slurmFile = open(repeatDir + sliceName + ".slurm", "w")
-        for line in lines:
-            slurmFile.write(line + "\n")
-        slurmFile.close()
+    # Get files in setupDir
+    filePaths = glob.glob(setupDir + "/*")
 
-        # Exit statement when end of the library is reached
-        if exit:
-            break
+    # Create each repeat dirs, and populate them with files
+    for repeatDir in range(1, repeatNum + 1):
 
-    # Update the repeat number
-    repeat += 1
+        repeatDir = str(repeatDir)
+        # Create the directory if it doesn't already exist
+        if not os.path.exists(repeatDir):
+            os.makedirs(repeatDir)
+
+        # Copy each file into this new directory
+        for filePath in filePaths:
+            fileName = os.path.basename(filePath)
+            shutil.copy(filePath, repeatDir + "/" + fileName)
+
+
+def createSlices():
+    repeat = 1
+    # Loop over repeat directories
+    while repeat <= repeatNum:
+
+        # Initialize variables for this repeat
+
+        repeatDir = os.getcwd() + "/" + str(repeat) + "/"
+        if not os.path.exists(repeatDir):
+            os.makedirs(repeatDir)
+        upperLimit = 0
+        sliceCount = 0
+        exit = False
+
+        # Loop over the slices
+        while True:
+
+            # Update upperLimit and sliceCount
+            lowerLimit = upperLimit + 1
+            upperLimit += sliceSize
+            sliceCount += 1
+
+            # Exit statement of the loop, when
+            if upperLimit > libSize:
+                upperLimit = libSize
+                exit = True
+
+            # Create sliceName for job name and slurm file name
+            sliceName = projName + "_rep" + str(repeat) + "_sl" + str(upperLimit)
+
+            # Content of the slurm file to be created
+            lines = []
+            lines.append("#!/bin/bash")
+            lines.append("#SBATCH -p main")
+            lines.append("#SBATCH --ntasks=1")
+            lines.append("#SBATCH --mem-per-cpu=1024")
+            lines.append("#SBATCH --time=" + walltime)
+            lines.append("#SBATCH --job-name=" + sliceName)
+            lines.append("")
+            lines.append("ICMHOME=/vlsci/VR0024/tcoudrat/bin/icm-3.7-3b")
+            lines.append("$ICMHOME/icm64 -vlscluster $ICMHOME/_dockScan " + projName +
+                " thorough=" + thor +
+                " from=" + str(lowerLimit) +
+                " to=" + str(upperLimit) +
+                " >& " + projName + "_" + str(upperLimit) + ".ou")
+
+            # Create the slurm file in current repeat directory
+            slurmFile = open(repeatDir + sliceName + ".slurm", "w")
+            for line in lines:
+                slurmFile.write(line + "\n")
+            slurmFile.close()
+
+            # Exit statement when end of the library is reached
+            if exit:
+                break
+
+        # Update the repeat number
+        repeat += 1
+
+if  __name__ == "__main__":
+    main()
