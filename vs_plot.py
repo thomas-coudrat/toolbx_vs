6bf56b0eec79c9dde637fe51f9deaa2839cd9a11
Thomas Coudrat <thomas.coudrat@gmail.com>
2014-02-17 16:37:35 +1100

    Improved vs_plot with legend etc...

--- vs_plot.py
+++ vs_plot.py
@@ -4,27 +4,63 @@ from matplotlib import pyplot as plt
 import argparse
 import os
 
-descr = "Feed rocData (however many files), plots ROC curves"
-descr_rocPaths = "Provide rocDataFiles: data1.csv,data2.csv,data4.csv"
-parser = argparse.ArgumentParser(description=descr)
-parser.add_argument("rocPaths", help=descr_rocPaths, nargs="+")
-args = parser.parse_args()
-rocPaths = args.rocPaths
 
-for rocPath in rocPaths:
-    rocFile = open(rocPath, "r")
-    rocLines = rocFile.readlines()
-    rocFile.close()
+def main():
 
-    X = []
-    Y = []
-    rocName = os.path.basename(rocPath).replace(".csv", "")
-    for line in rocLines:
-        ll = line.split(",")
-        X.append(int(ll[0]))
-        Y.append(int(ll[1]))
+    rocPaths, gui = parseArgs()
 
-    plt.plot(X, Y, label=rocName)
+    plot(rocPaths, gui)
 
-plt.legend()
-plt.show()
+
+def parseArgs():
+    """
+    Parsing and returning arguments
+    """
+
+    descr = "Feed rocData (however many files), plots ROC curves"
+    descr_gui = "Use this flag to display plot: saves to .png by the default"
+    descr_rocPaths = "Provide rocDataFiles data1.csv data2.csv data4.csv"
+    parser = argparse.ArgumentParser(description=descr)
+    parser.add_argument("-gui", action="store_true", help=descr_gui)
+    parser.add_argument("rocPaths", help=descr_rocPaths, nargs="+")
+
+    args = parser.parse_args()
+    rocPaths = args.rocPaths
+    gui = args.gui
+
+    return rocPaths, gui
+
+
+def plot(rocPaths, gui):
+    """
+    Plot the data provided as argument, to draw ROC curves
+    """
+
+    for rocPath in rocPaths:
+        rocFile = open(rocPath, "r")
+        rocLines = rocFile.readlines()
+        rocFile.close()
+
+        X = []
+        Y = []
+        rocName = os.path.basename(rocPath).replace(".csv", "")
+        for line in rocLines:
+            ll = line.split(",")
+            X.append(int(ll[0]))
+            Y.append(int(ll[1]))
+
+        plt.plot(X, Y, label=rocName)
+
+    plt.xlabel("Total count (" + str(X[-1]) + ")", fontsize=16)
+    plt.ylabel("True positive count (" + str(Y[-1]) + ")", fontsize=16)
+    plt.title("Recovery of known actives (ROC curve)")
+    plt.legend(loc="lower right", prop={'size': 12})
+
+    if gui:
+        plt.show()
+    else:
+        plt.savefig('rocPlot.png', bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    main()
