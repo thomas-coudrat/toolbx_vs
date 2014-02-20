66086b20e939e7d8c1fe208d400d1e2388e1e5f2
Thomas Coudrat <thomas.coudrat@gmail.com>
2014-02-20 15:03:30 +1100

    Made it executable on VLSCI and Desktop (by checking for presence of hard-coded script and ICM executable)

--- vs_index.py
+++ vs_index.py
@@ -20,39 +20,91 @@ import shutil
 import sys
 from subprocess import check_output, STDOUT, CalledProcessError
 
-# Path to the blank .icm file
-indexFile = "/vlsci/VR0024/tcoudrat/Scripts/vs_scripts/index.icm"
-icmExec = "/vlsci/VR0024/tcoudrat/bin/icm-3.7-3b/icm64"
 
-# Argument parsing stuff
-descr = "Create a .inx file of a .sdf file for VS"
-descr_sdf = "Provide a .sdf library to create a .inx file for"
-parser = argparse.ArgumentParser(description=descr)
-parser.add_argument("sdf", help=descr_sdf)
-args = parser.parse_args()
-sdfFile = args.sdf
+def main():
+    """
+    Run the script
+    """
 
-print
-print "CREATING .INX FOR:", sdfFile
-print
+    # Get the right paths
+    icm, script = setPaths()
 
-# Get working directory
-workDir = os.getcwd()
-sdfPath = workDir + "/" + sdfFile
-inxPath = sdfPath.replace(".sdf", ".inx")
+    # Extract the arguments
+    sdfFile = getArgs()
 
-# Copy the temp .icm script, modify it, run it, delete it
-# Copy
-shutil.copy(indexFile, "./temp.icm")
-# Modify
-os.system("sed -e 's|SDF_LIB|" + sdfPath + "|g' ./temp.icm -i")
-os.system("sed -e 's|INX_FILE|" + inxPath + "|g' ./temp.icm -i")
-# Execute
-try:
-    check_output(icmExec + " -s ./temp.icm", stderr=STDOUT, shell=True)
-except CalledProcessError, e:
-    print "\n Error executin the ICM script"
-    print e.output
-    sys.exit()
-# Delete
-os.remove("./temp.icm")
+    # Execute the script
+    executeScript(icm, script, sdfFile)
+
+
+def setPaths():
+    """
+    Determine which platform this is ran on based on the access to the scripts
+    """
+    # Path to the blank .icm file
+    scriptVlsci = "/vlsci/VR0024/tcoudrat/Scripts/vs_scripts/index.icm"
+    icmVlsci = "/vlsci/VR0024/tcoudrat/bin/icm-3.7-3b/icm64"
+    scriptDesktop = "/home/thomas/Copy/Tools/vs_scripts/index.icm"
+    icmDesktop = "/usr/icm-3.7-3b/icm64"
+
+    # Select the proper scripts
+    if os.path.exists(scriptVlsci):
+        script = scriptVlsci
+        icm = icmVlsci
+    elif os.path.exists(scriptDesktop):
+        script = scriptDesktop
+        icm = icmDesktop
+    else:
+        print "Script or icm executable not found"
+        sys.exit()
+
+    return icm, script
+
+
+def getArgs():
+    """
+    Argument parsing stuff
+    """
+
+    descr = "Create a .inx file of a .sdf file for VS"
+    descr_sdf = "Provide a .sdf library to create a .inx file for"
+    parser = argparse.ArgumentParser(description=descr)
+    parser.add_argument("sdf", help=descr_sdf)
+    args = parser.parse_args()
+    sdfFile = args.sdf
+
+    return sdfFile
+
+
+def executeScript(icm, script, sdfFile):
+    """
+    Execute the index.icm script
+    """
+
+    print
+    print "CREATING .INX FOR:", sdfFile
+    print
+
+    # Get working directory
+    workDir = os.getcwd()
+    sdfPath = workDir + "/" + sdfFile
+    inxPath = sdfPath.replace(".sdf", ".inx")
+
+    # Copy the temp .icm script, modify it, run it, delete it
+    # Copy
+    shutil.copy(script, "./temp.icm")
+    # Modify
+    os.system("sed -e 's|SDF_LIB|" + sdfPath + "|g' ./temp.icm -i")
+    os.system("sed -e 's|INX_FILE|" + inxPath + "|g' ./temp.icm -i")
+    # Execute
+    try:
+        check_output(icm + " -s ./temp.icm", stderr=STDOUT, shell=True)
+    except CalledProcessError, e:
+        print "\n Error executin the ICM script"
+        print e.output
+        sys.exit()
+    # Delete
+    os.remove("./temp.icm")
+
+
+if __name__ == "__main__":
+    main()
