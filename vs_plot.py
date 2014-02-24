b448fd594495f538029328538cc8ea053850f14d
Thomas Coudrat <thomas.coudrat@gmail.com>
2014-02-24 19:42:06 +1100

    Added possibility to write in percentage, and added title in arguments

--- vs_plot.py
+++ vs_plot.py
@@ -7,9 +7,13 @@ import os
 
 def main():
 
-    rocPaths, gui = parseArgs()
+    gui, title, rocPaths = parseArgs()
 
-    plot(rocPaths, gui)
+    # Plot the values 0 and 1, which correspond to X and Y numbers
+    plot(title, rocPaths, gui, "number")
+
+    # Plot the values 2 and 3, which correspond to the percentage X and Y
+    plot(title, rocPaths, gui, "percent")
 
 
 def parseArgs():
@@ -19,22 +23,35 @@ def parseArgs():
 
     descr = "Feed rocData (however many files), plots ROC curves"
     descr_gui = "Use this flag to display plot: saves to .png by the default"
+    descr_title = "Provide a title for the graph, also used as filename"
     descr_rocPaths = "Provide rocDataFiles data1.csv data2.csv data4.csv"
     parser = argparse.ArgumentParser(description=descr)
     parser.add_argument("-gui", action="store_true", help=descr_gui)
+    parser.add_argument("title", help=descr_title)
     parser.add_argument("rocPaths", help=descr_rocPaths, nargs="+")
 
     args = parser.parse_args()
-    rocPaths = args.rocPaths
     gui = args.gui
+    title = args.title
+    rocPaths = args.rocPaths
 
-    return rocPaths, gui
+    return gui, title, rocPaths
 
 
-def plot(rocPaths, gui):
+def plot(title, rocPaths, gui, mode):
     """
     Plot the data provided as argument, to draw ROC curves
     """
+
+    plt.figure()
+
+    # Pick the numbers corresponding to the mode selected
+    if mode == "number":
+        a = 0
+        b = 1
+    elif mode == "percent":
+        a = 2
+        b = 3
 
     for rocPath in rocPaths:
         rocFile = open(rocPath, "r")
@@ -46,20 +63,21 @@ def plot(rocPaths, gui):
         rocName = os.path.basename(rocPath).replace(".csv", "")
         for line in rocLines:
             ll = line.split(",")
-            X.append(int(ll[0]))
-            Y.append(int(ll[1]))
+            X.append(float(ll[a]))
+            Y.append(float(ll[b]))
 
         plt.plot(X, Y, label=rocName)
 
     plt.xlabel("Total count (" + str(X[-1]) + ")", fontsize=16)
     plt.ylabel("True positive count (" + str(Y[-1]) + ")", fontsize=16)
-    plt.title("Recovery of known actives (ROC curve)")
+    plt.title(title)
     plt.legend(loc="lower right", prop={'size': 12})
 
     if gui:
         plt.show()
     else:
-        plt.savefig('rocPlot.png', bbox_inches="tight")
+        fileName = title.replace(" ", "_") + "_" + mode + ".png"
+        plt.savefig(fileName, bbox_inches="tight")
 
 
 if __name__ == "__main__":
