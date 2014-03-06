5e494ad60e155498329e640836fccc10dd32aed7
Thomas Coudrat <thomas.coudrat@gmail.com>
2014-03-06 17:03:49 +1100

    Added option of -pbs or -slurm queuing system for submission

--- vs_submit.py
+++ vs_submit.py
@@ -3,9 +3,9 @@
 #----------------------------------------------------
 #
 #   Execute within a VS directory, will crawl through
-#   all its subdirs and submit all .slurm files found
-#   there, while pausing for 1 second between each
-#   submission
+#   all its subdirs and submit all .slurm or .pbs
+#   files found there, while pausing for 1 second
+#   between each submission
 #
 #   Thomas Coudrat, February 2014
 #
@@ -13,40 +13,92 @@
 
 import os
 import time
+import argparse
+import sys
 
-workDir = os.getcwd()
-slurmPaths = []
 
-# Listing direct subdirectories to the dir where this was executed
-for subDir in os.listdir(workDir):
-    if subDir.isdigit():
-        path = os.path.join(workDir, subDir)
+def main():
+    """
+    Run the VS submission script
+    """
 
-        # If this is a directory, then list the files/directories in it
-        if os.path.isdir(path):
-            #print path
-            files = os.listdir(path)
+    # Return the queuing system chosen
+    queue = parsing()
 
-            # For each of these, save every file that ends with .slurm in
-            # a list, by saving its full path
-            for file in files:
-                if file.endswith(".slurm"):
-                    slurmPaths.append(os.path.join(path, file))
+    workDir = os.getcwd()
+    queuePaths = []
 
-print
-print "SUBMITTING", str(len(slurmPaths)), "JOBS:"
-print
+    # Listing direct subdirectories to the dir where this was executed
+    for subDir in os.listdir(workDir):
+        if subDir.isdigit():
+            path = os.path.join(workDir, subDir)
 
-# Loop over the saved .slurm paths
-for slurmPath in slurmPaths:
+            # If this is a directory, then list the files/directories in it
+            if os.path.isdir(path):
+                #print path
+                files = os.listdir(path)
 
-    slurmDir = os.path.dirname(slurmPath)
-    slurmFile = os.path.basename(slurmPath)
-    #print slurmPath, slurmDir
+                # For each of these, save every file that ends with .slurm or
+                # .pbs in a list, by saving its full path
+                for file in files:
+                    if file.endswith(queue):
+                        queuePaths.append(os.path.join(path, file))
 
-    # Change to that repeat directory
-    os.chdir(slurmDir)
-    os.system("sbatch " + slurmFile)
-    time.sleep(1)
+    print
+    print "SUBMITTING", str(len(queuePaths)), "JOBS:"
+    print
 
-print
+    # Loop over the saved .slurm or .pbs paths
+    for queuePath in queuePaths:
+
+        queueDir = os.path.dirname(queuePath)
+        queueFile = os.path.basename(queuePath)
+        #print queuePath, queueDir
+
+        # Change to that repeat directory
+        os.chdir(queueDir)
+        # And submit using either SLURM or PBS queueing
+        # system depending on what was chosen
+        if queue == ".slurm":
+            #print "sbatch " + queueFile
+            os.system("sbatch " + queueFile)
+        elif queue == ".pbs":
+            #print "qsub " + queueFile
+            os.system("qsub " + queueFile)
+        time.sleep(1)
+
+    print
+
+
+def parsing():
+    """
+    Define arguments, parse and return them
+    """
+    descr = "Submits a VS using either -slurm or -pbs queuing system"
+    descr_pbs = "Optional argument, use the PBS queuing system"
+    descr_slurm = "Optional argument, use the SLURM queuing system"
+    parser = argparse.ArgumentParser(description=descr)
+    parser.add_argument("-pbs", action="store_true", help=descr_pbs)
+    parser.add_argument("-slurm", action="store_true", help=descr_slurm)
+    args = parser.parse_args()
+
+    pbs = args.pbs
+    slurm = args.slurm
+
+    # Check if a queuing system was chosen
+    if not pbs and not slurm:
+        print "A queuing system -pbs or -slurm must be chosen"
+        sys.exit()
+    elif pbs and slurm:
+        print "Only one of -pbs or -slurm option must be chosen"
+        sys.exit()
+    elif pbs:
+        queue = ".pbs"
+    elif slurm:
+        queue = ".slurm"
+
+    return queue
+
+
+if __name__ == "__main__":
+    main()
