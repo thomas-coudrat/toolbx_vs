b7cf1bd4cd94f7af7edaf3c1bef7101214185a63
Thomas Coudrat <thomas.coudrat@gmail.com>
2014-03-17 15:18:46 +1100

    Added vsDir as argument to vs_submit, and split the jobs into functions for clarity

--- vs_submit.py
+++ vs_submit.py
@@ -23,30 +23,43 @@ def main():
     """
 
     # Return the queuing system chosen
-    queue = parsing()
+    vsDir, queue = parsing()
 
-    workDir = os.getcwd()
+    # Store all queueing scripts to be submitted in this directory
+    queuePaths = getQueueScripts(vsDir, queue)
+
+    print "\n SUBMITTING", str(len(queuePaths)), "JOBS: \n"
+
+    # Submit all those scripts (using the proper queueing system)
+    submitQueueScripts(queuePaths, queue)
+
+    print
+
+
+def getQueueScripts(vsDir, queue):
+    """
+    Make a list of the scripts to be submited
+    """
     queuePaths = []
-
     # Listing direct subdirectories to the dir where this was executed
-    for subDir in os.listdir(workDir):
+    for subDir in os.listdir(vsDir):
         if subDir.isdigit():
-            path = os.path.join(workDir, subDir)
+            path = os.path.join(vsDir, subDir)
+            files = os.listdir(path)
 
-            # If this is a directory, then list the files/directories in it
-            if os.path.isdir(path):
-                #print path
-                files = os.listdir(path)
+            # For each of these, save every file that ends with .slurm or
+            # .pbs in a list, by saving its full path
+            for file in files:
+                if file.endswith(queue):
+                    queuePaths.append(os.path.join(path, file))
 
-                # For each of these, save every file that ends with .slurm or
-                # .pbs in a list, by saving its full path
-                for file in files:
-                    if file.endswith(queue):
-                        queuePaths.append(os.path.join(path, file))
+    return queuePaths
 
-    print
-    print "SUBMITTING", str(len(queuePaths)), "JOBS:"
-    print
+
+def submitQueueScripts(queuePaths, queue):
+    """
+    Submit all the queueing scripts
+    """
 
     # Loop over the saved .slurm or .pbs paths
     for queuePath in queuePaths:
@@ -67,21 +80,22 @@ def main():
             os.system("qsub " + queueFile)
         time.sleep(1)
 
-    print
-
 
 def parsing():
     """
     Define arguments, parse and return them
     """
     descr = "Submits a VS using either -slurm or -pbs queuing system"
+    descr_vsDir = "VS directory to be submitted to the queue"
     descr_pbs = "Optional argument, use the PBS queuing system"
     descr_slurm = "Optional argument, use the SLURM queuing system"
     parser = argparse.ArgumentParser(description=descr)
+    parser.add_argument("vsDir", help=descr_vsDir)
     parser.add_argument("-pbs", action="store_true", help=descr_pbs)
     parser.add_argument("-slurm", action="store_true", help=descr_slurm)
     args = parser.parse_args()
 
+    vsDir = args.vsDir
     pbs = args.pbs
     slurm = args.slurm
 
@@ -97,7 +111,7 @@ def parsing():
     elif slurm:
         queue = ".slurm"
 
-    return queue
+    return vsDir, queue
 
 
 if __name__ == "__main__":
