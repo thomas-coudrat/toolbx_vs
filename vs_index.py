65ac90adff6c1f3b6cf01fe978f2a011d1da7610
Thomas Coudrat <thomas.coudrat@gmail.com>
2014-08-18 18:09:24 +1000

    Added JSON support for ICM executable definition, integrated into vs_index.py. Also integrated .icm script creation within vs_index.py

--- vs_index.py
+++ vs_index.py
@@ -7,19 +7,18 @@
 #   is created in the same directory as .sdf, with the same
 #   name but with the .inx extension. It can then be moved to
 #   the VS directory.
-#   A blank .icm file is copied temporarly, modified temp
-#   and exectuted, then deleted.
+#   An .icm script is created temporarly, modified and exectuted, then deleted.
 #
-#   Thomas Coudrat, February 2014
+#   Thomas Coudrat, August 2014
 #
 # -------------------------------------------------------------
 
 import argparse
 import os
-import shutil
 import sys
 from subprocess import check_output, STDOUT, CalledProcessError
 import socket
+import json
 
 
 def main():
@@ -27,55 +26,49 @@ def main():
     Run the script
     """
 
-    # Get the right paths
-    icm, script = setPaths()
+    # Get the path from the Json file
+    icm = getPath()
 
     # Extract the arguments
-    sdfFile = getArgs()
+    sdfFile = parseArgs()
+
+    # Generate the ICM script
+    scriptPath = generateScript(icm, sdfFile)
 
     # Execute the script
-    executeScript(icm, script, sdfFile)
+    executeScript(icm, scriptPath, sdfFile)
 
 
-def setPaths():
+def getPath():
     """
-    Determine which platform this is ran on based on the access to the scripts
+    Read in the json file containing the ICM executable paths for each machine
+    Return the ICM executable corresponding to the hostname of the machine being
+    used currently
     """
-    # Path to the .icm script and icm executable
-    scriptVlsci = "/vlsci/VR0024/tcoudrat/Scripts/toolbx_vs/index.icm"
-    icmVlsci = "/vlsci/VR0024/tcoudrat/bin/icm-3.7-3b/icm64"
 
-    scriptLocal = "/home/thomas/Copy/toolbx_vs/index.icm"
-    icmDesktop = "/usr/icm-3.7-3b/icm64"
-    icmLaptop = "/home/thomas/bin/icm-3.8-0/icm64"
+    # This Json file stores the ICM executable locations for each platform
+    icmExecJson = os.path.dirname(os.path.realpath(__file__)) + "/icm_exec.json"
 
-    scriptMcc = "/nfs/home/hpcpharm/tcoudrat/Scripts/toolbx_vs/index.icm"
-    icmMcc = "/nfs/home/hpcpharm/tcoudrat/bin/icm-3.7-3b/icm64"
+    # Read content of .json file
+    with open(icmExecJson, "r") as jsonFile:
+        icmExec = json.load(jsonFile)
 
     # Get the hostname to know which computer this is executed on
     hostname = socket.gethostname()
 
-    # Select the proper scripts
-    if hostname == "barcoo":
-        script = scriptVlsci
-        icm = icmVlsci
-    elif hostname == "linux-T1650":
-        script = scriptLocal
-        icm = icmDesktop
-    elif hostname == "Ideapad":
-        script = scriptLocal
-        icm = icmLaptop
-    elif hostname == "msgln6.its.monash.edu.au":
-        script = scriptMcc
-        icm = icmMcc
+    # Assign the ICM executable path corresponding to the hostname, if it is not
+    # defined then stop the execution
+    if hostname in icmExec.keys():
+        icm = icmExec[hostname]
     else:
-        print "Script or icm executable not found"
+        print("The ICM executable is not defined for this machine, please edit\
+            the icm_exec.json file")
         sys.exit()
 
-    return icm, script
+    return icm
 
 
-def getArgs():
+def parseArgs():
     """
     Argument parsing stuff
     """
@@ -90,7 +83,37 @@ def getArgs():
     return sdfFile
 
 
-def executeScript(icm, script, sdfFile):
+def generateScript(icm, sdfFile):
+    """
+    Generate the ICM script in the current working directory
+    Modify its content to apply to the .sdf and .inx files
+    """
+
+    # Write the script
+    script_string = """#!ICM_EXEC
+call "_startup"
+
+# Create the .inx file, that indexes this database
+makeIndexChemDb "SDF_LIB" "INX_FILE" "mol" { "ID" }
+
+quit
+"""
+    workDir = os.getcwd()
+    scriptPath = workDir + "/temp.icm"
+    with open(scriptPath, "w") as script_file:
+        script_file.write(script_string)
+
+    # Modify the script
+    sdfPath = workDir + "/" + sdfFile
+    inxPath = sdfPath.replace(".sdf", ".inx")
+    os.system("sed -e 's|SDF_LIB|" + sdfPath + "|g' ./temp.icm -i")
+    os.system("sed -e 's|INX_FILE|" + inxPath + "|g' ./temp.icm -i")
+    os.system("sed -e 's|ICM_EXEC|" + icm + "|g' ./temp.icm -i")
+
+    return scriptPath
+
+
+def executeScript(icm, scriptPath, sdfFile):
     """
     Execute the index.icm script
     """
@@ -98,21 +121,9 @@ def executeScript(icm, script, sdfFile):
     print
     print "CREATING .INX FOR:", sdfFile
     print
-
-    # Get working directory
-    workDir = os.getcwd()
-    sdfPath = workDir + "/" + sdfFile
-    inxPath = sdfPath.replace(".sdf", ".inx")
-
-    # Copy the temp .icm script, modify it, run it, delete it
-    # Copy
-    shutil.copy(script, "./temp.icm")
-    # Modify
-    os.system("sed -e 's|SDF_LIB|" + sdfPath + "|g' ./temp.icm -i")
-    os.system("sed -e 's|INX_FILE|" + inxPath + "|g' ./temp.icm -i")
     # Execute
     try:
-        check_output(icm + " -s ./temp.icm", stderr=STDOUT, shell=True)
+        check_output(icm + " -s " + scriptPath, stderr=STDOUT, shell=True)
     except CalledProcessError, e:
         print "\n Error executing the ICM script"
         print e.output
